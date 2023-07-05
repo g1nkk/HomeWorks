4c72d8916507a0f78b1df9a47819779261a9c065
Gink83 <burlakak222@gmail.com>
2023-07-05 15:26:17 +0300

    05.07.2023

---
+++ MostDifficultDZ/MostDifficultDZ/MainWindow.xaml.cs
@@ -0,0 +1,3 @@
+﻿using System;
+using System.Windows;
+namespace MostDifficultDZ{public partial class MainWindow : Window { string[] texts = new string[] { "hello", "hewwo", "hi", "zdarova", "?" }; public MainWindow(){InitializeComponent();}private void Button_Click(object sender, RoutedEventArgs e){while(true){MessageBox.Show(texts[new Random().Next(texts.Length)]);}}}}
